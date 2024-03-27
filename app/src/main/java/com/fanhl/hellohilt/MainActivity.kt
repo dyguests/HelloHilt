41d4d49f17fd1d2be8b38c83f9183fdb5416352a
fanhl <dyguests@qq.com>
2024-03-27 15:44:31 +0800

    测试hiltViewModel生命周期

--- app/src/main/java/com/fanhl/hellohilt/MainActivity.kt
+++ app/src/main/java/com/fanhl/hellohilt/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -32,7 +33,15 @@ class MainActivity : ComponentActivity() {
                     val viewModel = hiltViewModel<MailViewModel>()
                     LazyColumn {
                         items(List(100) { it }) {
-                            Greeting("Item$it", viewModel)
+                            Card(
+                                modifier = Modifier.padding(8.dp)
+                            ) {
+                                if (it < 50) {
+                                    Greeting("Item$it", viewModel)
+                                } else {
+                                    Hello("Item$it")
+                                }
+                            }
                         }
                     }
                 }
@@ -43,7 +52,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, viewModel: MailViewModel) {
-    //    val viewModel = hiltViewModel<MailViewModel>()
+    Text(
+        text = "Greeting $name ${viewModel.hi}!",
+        modifier = Modifier.padding(8.dp)
+    )
+}
+
+@Composable
+fun Hello(name: String) {
+    val viewModel = hiltViewModel<MailViewModel>()
     Text(
         text = "Hello $name ${viewModel.hi}!",
         modifier = Modifier.padding(8.dp)
